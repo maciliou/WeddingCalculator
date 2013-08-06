4d2eca6e00bfea78fbf0a529649ca18e9bf3dd06
htteng1976 <htteng1976@gmail.com>
2013-08-06 12:18:38 +0800

    !Support Page5 WebView & Support URL link Page 4
    Revert the sequence of page 4 and page 5.

--- src/com/bezier/app/weddingcalculator/Fragment5.java
+++ src/com/bezier/app/weddingcalculator/Fragment5.java
@@ -1,34 +1,47 @@
 package com.bezier.app.weddingcalculator;
 
 import android.support.v4.app.Fragment;
-import android.annotation.SuppressLint;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
+import android.app.Activity;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources.NotFoundException;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.widget.TextView;
 
-@SuppressLint("SetJavaScriptEnabled")
 public class Fragment5 extends Fragment {
 
-	private String myURL = "http://www.google.com/";
-	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		
-		WebView myBrowser = (WebView)inflater.inflate(R.layout.fragment5, container, false);
-        WebSettings websettings = myBrowser.getSettings();  
-        websettings.setSupportZoom(true);  
-        websettings.setBuiltInZoomControls(true);   
-        websettings.setJavaScriptEnabled(true);  
-         
-        myBrowser.setWebViewClient(new WebViewClient());  
-        myBrowser.loadUrl(myURL);  
-        
-		return myBrowser;
+		View view = inflater.inflate(R.layout.fragment5, container, false);
+		TextView textView = (TextView)view.findViewById(R.id.version);
+		Activity activity = getActivity();
+		
+		// Display the version
+		try {
+			textView.setText("¥Ø«eª©¥»¡Gv" + activity.getPackageManager().getPackageInfo(activity.getBaseContext().getPackageName(), 0).versionName);
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		TextView textViewTitle1 = (TextView)view.findViewById(R.id.title1);
+		SpannableString s = null;
+		try {
+			s = new SpannableString(activity.getResources().getText(R.string.fragment4Title1Content));
+		} catch (NotFoundException e) {
+			s = new SpannableString("");
+		}
+	    Linkify.addLinks(s, Linkify.ALL);
+	    textViewTitle1.setMovementMethod(LinkMovementMethod.getInstance());	    
+	    textViewTitle1.setText(s);		
+		
+		return view;
 	}
 
 	@Override
