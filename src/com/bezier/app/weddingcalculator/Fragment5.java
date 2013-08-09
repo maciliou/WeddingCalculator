2c8eef916814bada5e99934f4242c9cb87ab739b
htteng1976 <htteng1976@gmail.com>
2013-08-09 14:26:58 +0800

    +Add OnClickListener() to Fragment5 button

--- src/com/bezier/app/weddingcalculator/Fragment5.java
+++ src/com/bezier/app/weddingcalculator/Fragment5.java
@@ -5,23 +5,29 @@ import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
 import android.text.util.Linkify;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources.NotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Fragment5 extends Fragment {
 
+	private Activity activity;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		
+		activity = getActivity();
+		
 		View view = inflater.inflate(R.layout.fragment5, container, false);
 		TextView textView = (TextView)view.findViewById(R.id.version);
-		Activity activity = getActivity();
 		
 		// Display the version
 		try {
@@ -40,7 +46,19 @@ public class Fragment5 extends Fragment {
 	    Linkify.addLinks(s, Linkify.ALL);
 	    textViewTitle1.setMovementMethod(LinkMovementMethod.getInstance());	    
 	    textViewTitle1.setText(s);		
-		
+	      
+	    Button button = (Button)view.findViewById(R.id.loanButton);
+	    button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				intent.setAction(Intent.ACTION_VIEW);
+				intent.addCategory(Intent.CATEGORY_BROWSABLE);
+				intent.setData(Uri.parse(activity.getResources().getText(R.string.fragment5ButtonLink).toString()));
+				startActivity(intent);
+			}
+	    });
+	    
 		return view;
 	}
 
